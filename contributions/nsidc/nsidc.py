2bc398b518e45868ec3dee8642bb42510715ee2d
Kevin Pluck <kevinp@micad.co.uk>
2017-09-17 11:49:36 +0100

    Some error handling

--- contributions/nsidc/nsidc.py
+++ contributions/nsidc/nsidc.py
@@ -9,5 +9,5 @@ def get():
     shared.download_data("ftp://sidads.colorado.edu/DATASETS/NOAA/G02135/north/daily/data/N_seaice_extent_daily_v2.1.csv")
     print("nsidc finished")
 
-    
+
     #ftp.sendcmd("MDTM N_seaice_extent_daily_v2.1.csv")
--- contributions/shared.py
+++ contributions/shared.py
@@ -4,14 +4,30 @@ import urllib.request
 from urllib.parse import urlparse
 import inspect
 import hashlib
-from ftplib import FTP
+import ftplib
+import socket
 
 def download_data(url):
     "Downloads file if modified since last download"
-    if _modified_since_last_download(url):
+
+    modified, error = _modified_since_last_download(url)
+
+    if modified:
         _download_data_file(url)
+        return
+
+    short_url = _shorten_url(url)
+
+    if error:
+        print("Error - could not download " + short_url)
     else:
-        print("Latest file already downloaded.")
+        print(short_url + " not modified since last download")
+
+def _shorten_url(url):
+    if len(url) > 70:
+        return url[:30] + "..." + url[-30:]
+
+    return url
 
 def _download_data_file(url):
     calling_module_folder = _get_calling_module_folder()
@@ -43,10 +59,25 @@ def _get_calling_module_folder():
     return calling_module_folder
 
 def _modified_since_last_download(url):
-    last_modified = _get_last_modified(url)
+
+    try:
+        last_modified = _get_last_modified(url)
+    except urllib.error.URLError as exception:
+        print(exception.reason)
+        return (False, True)
+    except ftplib.error_perm as exception:
+        print(exception)
+        print("file likely doesn't exist - check filename")
+        return (False, True)
+    except socket.gaierror as exception:
+        print(exception)
+        print("Ftp hostname likely doesn't exist - check hostname")
+        return (False, True)
+
+
     previous_last_modified = _get_previous_last_modified(url)
 
-    return last_modified != previous_last_modified
+    return (last_modified != previous_last_modified, False)
 
 def _get_previous_last_modified(url):
     hash_filepath = _get_hash_filepath(url)
@@ -80,6 +111,7 @@ def _get_last_modified(url):
 def _get_last_modified_via_http(url):
     req = urllib.request.Request(url, method="HEAD")
     resp = urllib.request.urlopen(req)
+
     last_modified = resp.getheader("last-modified")
     return last_modified
 
@@ -87,10 +119,9 @@ def _get_last_modified_via_ftp(url):
     parsed_ftp_url = urlparse(url)
     ftp_host = parsed_ftp_url.hostname
     ftp_path = parsed_ftp_url.path
-    ftp = FTP(ftp_host)
+    ftp = ftplib.FTP(ftp_host)
     ftp.login()
     last_modified = str(ftp.sendcmd("MDTM " + ftp_path))
-    print("ftp last modified = " + last_modified)
     ftp.quit()
     return last_modified
 
@@ -103,7 +134,7 @@ def _retrieve_via_ftp(parsed_url, downloaded_filepath):
     ftp_path = parsed_url.path
 
     try:
-        ftp = FTP(ftp_host)
+        ftp = ftplib.FTP(ftp_host)
         ftp.login()
         ftp.retrbinary("RETR " + ftp_path, open(downloaded_filepath, "wb").write)
         last_modified = str(ftp.sendcmd("MDTM " + ftp_path))
