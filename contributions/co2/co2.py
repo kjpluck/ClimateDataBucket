6d0e33298db33bfcff02b53ca1ec5260b4feff59
Kevin Pluck <kevinjpluck@gmail.com>
2017-11-12 12:08:58 +0000

    renamed get to download in co2

--- contributions/co2/co2.py
+++ contributions/co2/co2.py
@@ -1,9 +1,9 @@
 'The co2 contribution'
 from contributions import shared
 
-def get():
+def download():
     'this is the doc string'
-    print("this is get() in co2")
+    print("this is download() in co2")
 
     shared.download_data("http://scrippsco2.ucsd.edu/assets/data/atmospheric/stations/in_situ_co2/weekly/weekly_in_situ_co2_mlo.csv")
 
